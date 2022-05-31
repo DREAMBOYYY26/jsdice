3460815f5982c6e3899bd0534b399b2be1858d35
DREAMBOYYY26 <102305077+DREAMBOYYY26@users.noreply.github.com>
2022-05-31 20:40:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
-
     let r1 = Math.floor(Math.random() * 5 + 1);
 
-    let r2 = Math.floor(Math.random() * 5 + 1);           
-
-    if (rand1 > rand2) {
-
-      document.write('| Player 1 Wins |');
-
-    } else if (rand1 < rand2) {
-
-      document.write('| Player 2 Wins |');
-
-    } else {
-
-      document.write('| Draw |');
-
-    }
+    let r2 = Math.floor(Math.random() * 5 + 1);  
+         
+    if (rand1 > rand2) { document.write('| Player 1 Wins |');}
+      else if (rand1 < rand2) { document.write('| Player 2 Wins |'); } 
+      else { document.write('| Draw |');  }
 
     document.write(`<img src="/dice${r1}.png">`);
 
